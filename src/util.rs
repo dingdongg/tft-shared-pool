2a2135f4ee03ad6b37e9ae96d5c3c7615d2ef4c6
dingdongg <cs.dg.kim@gmail.com>
2023-12-26 17:58:46 -0700

    implement FromStr for traits enum

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::str::FromStr;
 
 #[derive(Debug)]
 pub enum TFTTrait {
@@ -39,11 +40,50 @@ impl fmt::Display for TFTTrait {
     }
 }
 
+impl FromStr for TFTTrait {
+    type Err = ();
+    fn from_str(input: &str) -> Result<TFTTrait, Self::Err> {
+        match input {
+            "8-bit" => Ok(Self::EightBit),
+            "Country" => Ok(Self::Country),
+            "Disco" => Ok(Self::Disco),
+            "EDM" => Ok(Self::Edm),
+            "Emo" => Ok(Self::Emo),
+            "Heartsteel" => Ok(Self::Heartsteel),
+            "Hyperpop" => Ok(Self::Hyperpop),
+            "ILLBEATS" => Ok(Self::Illbeats),
+            "Jazz" => Ok(Self::Jazz),
+            "K/DA" => Ok(Self::Kda),
+            "Maestro" => Ok(Self::Maestro),
+            "Mixmaster" => Ok(Self::Mixmaster),
+            "Pentakill" => Ok(Self::Pentakill),
+            "Punk" => Ok(Self::Punk),
+            "True Damage" => Ok(Self::TrueDamage),
+            "Wildcard" => Ok(Self::Wildcard),
+            "Big Shot" => Ok(Self::BigShot),
+            "Bruiser" => Ok(Self::Bruiser),
+            "Breakout" => Ok(Self::Breakout),
+            "Crowd Diver" => Ok(Self::CrowdDiver),
+            "Dazzler" => Ok(Self::Dazzler),
+            "Edgelord" => Ok(Self::Edgelord),
+            "Executioner" => Ok(Self::Executioner),
+            "Guardian" => Ok(Self::Guardian),
+            "Mosher" => Ok(Self::Mosher),
+            "Rapidfire" => Ok(Self::Rapidfire),
+            "Sentinel" => Ok(Self::Sentinel),
+            "Spellweaver" => Ok(Self::Spellweaver),
+            "Superfan" => Ok(Self::Superfan),
+            _ => Err(()),
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct Unit {
-    cost: u32,
-    name: String,
-    traits: Vec<TFTTrait>,
-    is_headliner: bool,
+    pub cost: u32,
+    pub name: String,
+    pub traits: Vec<TFTTrait>,
+    pub is_headliner: bool,
 }
 
 impl std::fmt::Display for Unit {
